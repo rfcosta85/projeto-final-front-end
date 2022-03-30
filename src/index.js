6a1f523fb3df956a56d587509cdf54ec02f655cf
unknown <rfcosta85@gmail.com>
2022-03-29 21:45:54 -0300

    First config on index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root"),
-);
+  document.getElementById('root')
+)
