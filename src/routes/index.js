77f579ff9daf57c8e3429a5d2ee72f2c5804d26e
unknown <rfcosta85@gmail.com>
2022-04-13 20:12:07 -0300

    Add logout router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-import { getFromStorage } from '../services/auth';
+import { getFromStorage } from '../services/auth'
 
 export const APP_ROUTES = [
   {
     name: 'Home',
-    icon: fill => (
+    icon: (fill) => (
       <svg
         width="21"
         height="17"
@@ -21,7 +21,7 @@ export const APP_ROUTES = [
   },
   {
     name: 'Meu petfil',
-    icon: fill => (
+    icon: (fill) => (
       <svg
         width="21"
         height="20"
@@ -39,7 +39,7 @@ export const APP_ROUTES = [
   },
   {
     name: 'Configurações',
-    icon: fill => (
+    icon: (fill) => (
       <svg
         width="19"
         height="20"
@@ -55,4 +55,27 @@ export const APP_ROUTES = [
     ),
     url: '/settings',
   },
-];
+
+  {
+    name: 'Sair',
+    icon: (fill) => (
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7.09 12.59L8.5 14L13.5 9L8.5 4L7.09 5.41L9.67 8H0V10H9.67L7.09 12.59ZM16 0H2C0.89 0 0 0.9 0 2V6H2V2H16V16H2V12H0V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0Z"
+          fill={fill}
+        />
+        <path
+          d="M7.09 12.59L8.5 14L13.5 9L8.5 4L7.09 5.41L9.67 8H0V10H9.67L7.09 12.59ZM16 0H2C0.89 0 0 0.9 0 2V6H2V2H16V16H2V12H0V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V2C18 0.9 17.1 0 16 0Z"
+          stroke="white"
+        />
+      </svg>
+    ),
+    url: '/login',
+  },
+]
